e3ad1c3e0fbdca13b094cae808139c4fc3bb0fb6
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-02-20 13:43:54 +0100

    næsten færdig – mangler at fikse animationer

--- js.js
+++ js.js
@@ -1,18 +1,46 @@
 const toDoListQsl = document.querySelector(".to_do_list");
 
-//Hardcodet array – skal være dynamisk med det som brugeren skriver ind
-const doDoArray = [
-  { id: self.crypto.randomUUID(), text: "Lave to do app", done: false, subtext: "deadline er torsdag kl. 23:59" },
-  { id: self.crypto.randomUUID(), text: "Rette opgaver", done: false },
-  { id: self.crypto.randomUUID(), text: "Gøre rent", done: true, subtext: "min tur til at støvsuge" },
-];
+//Tomt array
+const toDoArray = [];
 
-//Funktion som kører tasks ud til dommen
-sortedList();
+//Eventlistener på button
+document.querySelector(".input_button").addEventListener("click", addTaskToList);
+
+// Eventlistener på inputfelterne for Enter-tast
+document.querySelector(".input_text").addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    addTaskToList();
+  }
+});
+
+document.querySelector(".input_subtext").addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    addTaskToList();
+  }
+});
+
+function addTaskToList() {
+  let inputValue = document.querySelector(".input_text").value; //Henter værdien af input feltet
+  let subInputValue = document.querySelector(".input_subtext").value; //Henter værdien af andet input felt
+
+  document.querySelector(".input_text").value = ""; // Rydder teksten i input_feltet
+  document.querySelector(".input_subtext").value = ""; // Rydder teksten i subtext-feltet
+
+  const newObjToAddToArray = {
+    id: self.crypto.randomUUID(),
+    text: inputValue,
+    done: false,
+    subtext: subInputValue,
+  };
+
+  toDoArray.push(newObjToAddToArray);
+  sortedList();
+}
 
 //Funktion som sortere eller filtrere listen af tasks
 function sortedList() {
-  let listToShow = doDoArray;
+  let listToShow = toDoArray;
   listToShow.sort((a, b) => {
     if (a.done && !b.done) {
       return 1;
@@ -30,7 +58,7 @@ function showToDo(listToShow) {
   listToShow.forEach((task) => {
     const li = document.createElement("li"); //Oprettet elementet li
 
-    li.innerHTML += `<input type="checkbox" ${task.done ? "checked" : ""} class="mark_toggle_done"><h3>${task.text}</h3><p>${task.subtext}</p><h5 class="delete">x</h5>`; //Skriver li ud i dommen – Hvis done = true sættes attributen checked (flueben) på input feltet, eller fjernes atributten
+    li.innerHTML += `<input type="checkbox" ${task.done ? "checked" : ""} class="mark_toggle_done"><div><h3>${task.text}</h3><p>${task.subtext}</p></div><h5 class="delete">x</h5>`; //Skriver li ud i dommen – Hvis done = true sættes attributen checked (flueben) på input feltet, eller fjernes atributten
     li.classList.add(task.done ? "color_done" : "color_task"); //Hvis done = true addes klassen color_done ellers tilføjes klassen color_task
 
     li.addEventListener("click", (evt) => {
@@ -41,10 +69,22 @@ function showToDo(listToShow) {
       console.log("current target", currentTarget);
       console.log("target", target);
 
-      //Opdatere arrayet
+      //Opdatere listen
       if (target.classList.contains("delete")) {
-        currentTarget.remove(currentTarget); //Fjerner currenttarget (hele liste elementet) ved at klikke på target (krydset)
+        document.querySelector("dialog").showModal();
+        document.querySelector(".ja").addEventListener("click", removeTask);
+        function removeTask() {
+          const taskId = task.id;
+          const index = toDoArray.findIndex((item) => item.id === taskId);
+          if (index !== -1) {
+            toDoArray.splice(index, 1);
+          }
+
+          currentTarget.remove();
+          sortedList();
+        }
       }
+
       if (target.classList.contains("mark_toggle_done")) {
         task.done = !task.done; //Gør den til det modsatte af hvad den er true/false
 
